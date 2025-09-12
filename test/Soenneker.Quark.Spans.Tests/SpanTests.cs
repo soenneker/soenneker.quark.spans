850194f68db0c5ba009860480bb1e0af574fc7ce
Jake Soenneker <jake@soenneker.com>
2025-09-11 20:18:33 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Spans.Tests/SpanTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Spans.Tests;
+
+[Collection("Collection")]
+public sealed class SpanTests : FixturedUnitTest
+{
+    public SpanTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
